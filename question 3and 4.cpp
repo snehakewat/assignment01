5cfbf80d6d9d35f57f06b7c36674786f2b82543c
snehakewat <159766627+snehakewat@users.noreply.github.com>
2024-03-15 17:33:56 +0530

    Add files via upload

---
+++ question 3and 4.cpp
@@ -0,0 +1,9 @@
+int   4 bytes 
+bool  1 bytes 
+float 4 bytes 
+
+
+
+
+output of the program is-
+6 4 
---
+++ question5and6.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main (){
+    int r;
+    r = 10;
+    float pi = 3.14;
+    float circumference = 2*pi*r;
+    cout<< circumference ;
+    return 0;
+}
+
+
+
+
+
+
+only 2 can be variable name-
+_FLOAT 
+FLOAT 
